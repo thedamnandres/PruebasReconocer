df49ee3410065d5141a1bcd3a5f59b622c2d3ff1
Carlos Javier Guagrilla <carlos.guagrilla.chicaiza@udla.edu.ec>
2025-04-15 09:56:42 -0500

    configuacionn de cors

--- ReconocerApp.API/Program.cs
+++ ReconocerApp.API/Program.cs
@@ -3,6 +3,9 @@ using ReconocerApp.API.Data;
 using ReconocerApp.API.Mappings; // 👈 Asegúrate de tener este using
 
 var builder = WebApplication.CreateBuilder(args);
+// Habilitar logs de consola
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services
 builder.Services.AddControllers();
@@ -24,16 +27,27 @@ else
     throw new Exception("Unsupported database provider specified in appsettings.json");
 }
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
-app.UseAuthorization();
 app.MapControllers();
-
+app.MapGet("/", () => "Welcome to the API!");
 app.Run();
+
